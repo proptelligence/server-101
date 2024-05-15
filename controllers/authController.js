5516931f93c6cce3643dcee19a1010c0c3f10bd3
Ajay Medidhi <info@proptelligence.net>
2024-05-15 18:37:57 +0530

    updated authentication

---
+++ controllers/authController.js
@@ -0,0 +1,35 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+exports.register = async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ name, email, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
+
+exports.login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        const validPassword = await bcrypt.compare(password, user.password);
+        if (!validPassword) {
+            return res.status(401).json({ error: 'Invalid email or password' });
+        }
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+};
--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-// Importing required modules
 const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors'); // Import cors module
+require('dotenv').config();
+const authRoutes = require('./routes/authRoutes');
 
-// Creating an Express application
 const app = express();
-const port = 3000; // Port number
 
-// Sample route for testing
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
+app.use(express.json());
 
-// Starting the server
-app.listen(port, () => {
-  console.log(`Server is listening at http://localhost:${port}`);
-});
+// Configure CORS
+const allowedOrigins = ['http://localhost:3000', 'https://www.proptelligence.net',"https://server-101.onrender.com"];
+app.use(cors({
+  origin: function (origin, callback) {
+    // Check if the origin is in the allowedOrigins array or if it is undefined (for server-to-server communication)
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
+
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch(err => console.error('Error connecting to MongoDB:', err));
+
+app.use('/', authRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
